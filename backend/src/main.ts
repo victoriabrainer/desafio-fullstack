f74abe0d855253adfb4414b03e9d325d11d6b5dd
Victoria Brainer <maria.vrb68@gmail.com>
2024-07-09 22:49:41 -0300

    Backend: Add Enable Cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Desafio Fullstack - Parvi')
     .setDescription('Cars API Parvi')
